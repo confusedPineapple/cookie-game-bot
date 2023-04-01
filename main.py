3b7cec59ea049d73286ecf6d6155041e2620970f
confusedPineapple <isabelxcoding@gmail.com>
2023-03-31 20:14:00 -0700

    A lot has happened, the program somewhat works but there are still errors with upgrading

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
 import time
 
-check = time.time() + 10
 
+check = time.time()
 
 
 
@@ -17,34 +17,50 @@ driver = webdriver.Chrome(service=service, options=options)
 driver.get('http://orteil.dashnet.org/experiments/cookie/')
 cookie = driver.find_element(By.ID,'cookie')
 
-i = True
-while i:
-    if time.time() > check:
-        print('in if statement')
-        money = driver.find_element(By.ID, 'money')
-        my_money = int(money.text)
-        upgrades = driver.find_element(By.ID, 'store')
-        upgraded_list = upgrades.find_elements(By.TAG_NAME, 'div')
-        upgrade_list_name_price = []
-        upgrade_list_price_only = []
-        upgrade_list_name_only = []
-        new_dict = {}
 
-        for i in upgraded_list:
-            upgrade_list_name_price.append(i.text.split('\n')[0])
-        upgrade_list_name_price.pop()
-        for i in upgrade_list_name_price:
-            upgrade_list_name_only.append(i.split(' ')[0])
-        for i in upgrade_list_name_price:
-            upgrade_list_price_only.append(int(i.split(' ')[-1].replace(',', '')))
-        new_dict = dict(zip(upgrade_list_name_only, upgrade_list_price_only))
-        for i in new_dict:
-            if my_money >= new_dict[i]:
-                upgrade = driver.find_element(By.ID, f'buy{i}')
-                upgrade.click()
-        count = time.time()
-    else:
-        cookie.click()
+
+check_num = 10
+j = True
+while j:
+    cookie.click()
+    check2 = check + check_num
+    time_now = time.time()
+    if time_now > check2:
+        try:
+            money = driver.find_element(By.ID, 'money')
+            my_money = int(money.text)
+            upgrades = driver.find_element(By.ID, 'store')
+            upgraded_list = upgrades.find_elements(By.TAG_NAME, 'div')
+            upgrade_list_name_price = []
+            upgrade_list_price_only = []
+            upgrade_list_name_only = []
+            new_dict = {}
+
+            for i in upgraded_list:
+                upgrade_list_name_price.append(i.text.split('\n')[0])
+
+            upgrade_list_name_price.pop()
+            for i in upgrade_list_name_price:
+                upgrade_list_name_only.append(i.split(' ')[0])
+
+            for i in upgrade_list_name_price:
+                upgrade_list_price_only.append(int(i.split(' ')[-1].replace(',', '')))
+
+
+
+            new_dict = dict(zip(upgrade_list_name_only, upgrade_list_price_only))
+            sorted_dict = dict(sorted(new_dict.items(),key=lambda item: item[1]))
+            print(sorted_dict)
+            for i in reversed(new_dict):
+                price = new_dict[i]
+                if my_money >= price:
+                    upgrade = driver.find_element(By.ID, f'buy{i}')
+                    upgrade.click()
+                    check_num += 30
+                    break
+        except:
+            break
+
 
 
 
