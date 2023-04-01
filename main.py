7b05527d81bd7cfd8b101e468c9a7478f90cb9a4
confusedPineapple <isabelxcoding@gmail.com>
2023-04-01 01:11:13 -0700

    Well it kind of works now, the upgrade error has been resolved for the most part. The comma in the money amout was literally destroying the program.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from selenium.webdriver.common.by import By
 import time
 
 
-check = time.time()
-
+start_time =  time.time()
+end_time =  time.time() + 20
 
 
 service = Service("C:\\Users\SOMA\Music\chromeDriver\chromedriver.exe")
@@ -23,43 +23,38 @@ check_num = 10
 j = True
 while j:
     cookie.click()
-    check2 = check + check_num
     time_now = time.time()
-    if time_now > check2:
-        try:
-            money = driver.find_element(By.ID, 'money')
-            my_money = int(money.text)
-            upgrades = driver.find_element(By.ID, 'store')
-            upgraded_list = upgrades.find_elements(By.TAG_NAME, 'div')
-            upgrade_list_name_price = []
-            upgrade_list_price_only = []
-            upgrade_list_name_only = []
-            new_dict = {}
+    if time_now > end_time:
+        money = driver.find_element(By.ID, 'money')
+        print(money.text)
+        my_money = int(money.text.replace(',', ''))
+        upgrades = driver.find_element(By.ID, 'store')
+        upgraded_list = upgrades.find_elements(By.TAG_NAME, 'div')
+        upgrade_list_name_price = []
+        upgrade_list_price_only = []
+        upgrade_list_name_only = []
+        new_dict = {}
 
-            for i in upgraded_list:
-                upgrade_list_name_price.append(i.text.split('\n')[0])
+        for i in upgraded_list:
+            upgrade_list_name_price.append(i.text.split('\n')[0])
 
-            upgrade_list_name_price.pop()
-            for i in upgrade_list_name_price:
-                upgrade_list_name_only.append(i.split(' ')[0])
+        upgrade_list_name_price.pop()
+        for i in upgrade_list_name_price:
+            upgrade_list_name_only.append(i.split(' ')[0])
 
-            for i in upgrade_list_name_price:
-                upgrade_list_price_only.append(int(i.split(' ')[-1].replace(',', '')))
+        for i in upgrade_list_name_price:
+            upgrade_list_price_only.append(int(i.split(' ')[-1].replace(',', '')))
+        new_dict = dict(zip(upgrade_list_name_only, upgrade_list_price_only))
+        sorted_dict = dict(sorted(new_dict.items(),key=lambda item: item[1]))
 
-
-
-            new_dict = dict(zip(upgrade_list_name_only, upgrade_list_price_only))
-            sorted_dict = dict(sorted(new_dict.items(),key=lambda item: item[1]))
-            print(sorted_dict)
-            for i in reversed(new_dict):
-                price = new_dict[i]
-                if my_money >= price:
-                    upgrade = driver.find_element(By.ID, f'buy{i}')
-                    upgrade.click()
-                    check_num += 30
-                    break
-        except:
-            break
+        print(sorted_dict)
+        for i in reversed(sorted_dict):
+            price = new_dict[i]
+            if my_money >= price:
+                upgrade = driver.find_element(By.ID, f'buy{i}')
+                upgrade.click()
+                end_time = time.time() + 20
+                break
 
 
 
